addce95c1e22c7fd41a9ea0986a714e8bede23cc
Erid Bardhaj <erid.bardhaj@gmail.com>
2016-08-21 00:05:07 +0200

    Add initial files for the screen

---
+++ MovieChallenge/Components/TableViewCells/PopularMoviesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PopularMoviesTableViewCell.swift
+//  MovieChallenge
+//
+//  Created by Erid Bardhaj on 8/21/16.
+//  Copyright © 2016 trivago GmbH. All rights reserved.
+//
+
+import UIKit
+
+class PopularMoviesTableViewCell: UITableViewCell {
+    
+    static let cellIdentifier = String(self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
