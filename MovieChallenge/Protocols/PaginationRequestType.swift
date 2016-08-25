f7fd4e56886632be1e2497a6060b1f40eeb72c8f
Erid Bardhaj <erid.bardhaj@gmail.com>
2016-08-25 21:53:58 +0200

    Add protocols to make API more protocol oriented

---
+++ MovieChallenge/Protocols/PaginationRequestType.swift
@@ -0,0 +1,16 @@
+//
+//  PaginationRequestType.swift
+//  MovieChallenge
+//
+//  Created by Erid Bardhaj on 8/25/16.
+//  Copyright © 2016 trivago GmbH. All rights reserved.
+//
+
+import Foundation
+
+protocol PaginationRequestType: RESTTargetType {
+    associatedtype Response: PaginationResponseType
+    var page: Int { get }
+    
+    func requestWithPage(page: Int) -> Self
+}
