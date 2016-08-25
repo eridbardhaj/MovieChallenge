6a6275e995687e3b0ce20c8dee2a98c897ddbf9d
Erid Bardhaj <erid.bardhaj@gmail.com>
2016-08-26 00:43:57 +0200

    Configure search movie request

--- MovieChallenge/Services/Networking/MovieRequest.swift
+++ MovieChallenge/Services/Networking/MovieRequest.swift
@@ -49,6 +49,49 @@ struct MovieRequest {
             return PopularList(page: page)
         }
     }
+    
+    struct SearchMovies: RESTTargetType, PaginationRequestType {
+        let query: String
+        let page: Int
+        
+        init(let query: String, page: Int = 1) {
+            self.query = query
+            self.page = page
+        }
+        
+        // MARK: - Protocol Conformance
+        
+        // MARK: RESTTargetType
+        
+        typealias Response = PaginationResponse<Movie>
+        
+        var path: String {
+            return "/search/movies"
+        }
+        
+        var method: Alamofire.Method {
+            return .GET
+        }
+        
+        var parameters: [String : AnyObject]? {
+            return ["extended": "full,images",
+                    "page": "\(page)",
+                    "limit": "10",
+                    "query": query]
+        }
+        
+        var extraHeaders: [String : String]? {
+            return ["Content-Type": "application/json",
+                    "trakt-api-version": "2",
+                    "trakt-api-key": Constants.API.key]
+        }
+        
+        // MARK: PaginationRequestType
+        
+        func requestWithPage(page: Int) -> SearchMovies {
+            return SearchMovies(query: query, page: page)
+        }
+    }
 }
 
 
